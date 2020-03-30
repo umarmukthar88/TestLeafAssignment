9509a274052497bdd82b76b45d6403d0a625db8d
umarmukthar88 <62899697+umarmukthar88@users.noreply.github.com>
2020-03-30 20:41:06 +0400

    ClassroomAssignment

---
+++ Mobile.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class Mobile {
+
+	
+	String brandName="Apple" ;
+	
+	boolean isSmart = true;
+	
+	char FirsName='A';		
+	
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		Mobile Mob=new Mobile();
+		
+		 String Mobbrnd=Mob.brandName;
+		 System.out.println(Mobbrnd);
+		
+		 
+		 char firname=Mob.FirsName;
+		 System.out.println(firname);
+		 
+		 boolean smart=Mob.isSmart;
+		 System.out.println(smart);
+		
+		
+		 
+		
+		
+		
+		System.out.println("i am ready");
+	}
+
+}
