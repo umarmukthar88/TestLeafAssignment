96f565e8c183538f590b298ea488e294e87470a4
umarmukthar88 <62899697+umarmukthar88@users.noreply.github.com>
2020-04-01 07:09:47 +0400

    Week1Da2Assignment1

---
+++ CreateLead.java
@@ -0,0 +1,58 @@
+package week1.day2;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead 
+
+
+{
+public static void main(String[] args) 
+	
+	
+	{
+
+		System.setProperty("webdriver.chrome.driver" , "./chromedriver.exe");
+		
+	
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		
+		//User Login
+		
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementByName("PASSWORD").sendKeys("crmsfa");
+		driver.findElementByClassName("decorativeSubmit").click();
+		
+		//Click CRM Link
+		driver.findElementByLinkText("CRM/SFA").click();
+		
+		//Enter Lead
+		
+		driver.findElementByLinkText("Create Lead").click();
+		driver.findElementById("createLeadForm_companyName").sendKeys("TestCompany");
+		driver.findElementById("createLeadForm_firstName").sendKeys("FirstName");
+		driver.findElementById("createLeadForm_lastName").sendKeys("LastName");
+		driver.findElementByName("submitButton").click();
+		
+		//getTitle
+		boolean elementDisplay=driver.findElementById("sectionHeaderTitle_leads").isDisplayed();
+		System.out.println("Title Found :"+ elementDisplay);
+
+		
+		//String title=driver.getTitle();
+		//System.out.println(title);
+		driver.findElementByLinkText("Logout").click();
+		
+			
+		driver.close();
+		
+		
+
+	}
+	
+}
