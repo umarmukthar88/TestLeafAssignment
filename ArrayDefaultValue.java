d5e86580fb0d8c95a4b7dcd7e07c2ef243ca6d62
umarmukthar88 <62899697+umarmukthar88@users.noreply.github.com>
2020-04-05 18:36:09 +0400

    week 1 day4 classassignment 6

---
+++ ArrayDefaultValue.java
@@ -0,0 +1,39 @@
+package week1.day4;
+
+public class ArrayDefaultValue 
+
+
+{
+	
+	
+		public static void main(String[] args) 
+		
+		
+		{
+			
+			short[] shr=new short[2];
+			System.out.println("The default value in short datatype is : "+shr[1]);
+						
+			int[] integer=new int[4];
+			System.out.println("The default value in int datatype is :"+integer[1]);
+			
+			long[] lng=new long[2];
+			System.out.println("The default value in Long datatype is :"+lng[1]);
+			
+			double[] dbl=new double[2];
+			System.out.println("The default value in double datatype is :"+dbl[1]);
+			
+			boolean[] bln=new boolean[2];
+			System.out.println("The default value in boolean datatype is :"+bln[1]);
+			
+			String [] Str=new String[4];
+			System.out.println("The default value in string datatype is :"+Str[1]);
+						
+			char [] chr=new char[4];
+			System.out.println("The default value in Char datatype is :"+chr[1]);
+			
+		
+			
+		}
+
+}
